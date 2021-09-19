ceee2bf28b13c9d03e8038798dbf7ae69530cfc1
SCA <70905913+scostas54@users.noreply.github.com>
2021-09-19 19:24:03 +0200

    Add new variables to send
    New variables on function "relay_control" added: 
    unsigned long deactpump_interval, unsigned long actpump_interval, int tempThreshold, int CO2Threshold, int humiThreshold

--- RelayControl.h
+++ RelayControl.h
@@ -1,5 +1,6 @@
 void relay_init();
-void relay_control(float humidity, float temperature, int ppm_CO2_interior);
+void relay_control(float humidity, float temperature, int ppm_CO2_interior, unsigned long deactpump_interval, unsigned long actpump_interval, int tempThreshold,
+                  , int CO2Threshold, int humiThreshold);
 void relay_config(byte pin_relay);
 void relay_activate(byte pin_relay);
 void relay_deactivate(byte pin_relay);
