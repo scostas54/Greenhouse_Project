ae1832e97bfcf3d51a4f9009b9e8ae3a8634d914
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-02 11:10:10 +0200

    Update RelayControl.h

--- RelayControl.h
+++ RelayControl.h
@@ -1,5 +1,5 @@
 void relay_init();
-void relay_control(float humidity, float temperature, int ppm_CO2_interior, unsigned long deactpump_interval, unsigned long actpump_interval, int tempThreshold,
+void relay_control(float internal_humidity, float internal_temperature, int internal_CO2, unsigned long deactpump_interval, unsigned long actpump_interval, int tempThreshold,
                   , int CO2Threshold, int humiThreshold);
 void relay_config(byte pin_relay);
 void relay_activate(byte pin_relay);
