3e15af5c14635ecae993020d4cca1a28c568c4ee
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-02 10:55:03 +0200

    External Values + PH added
    The values for external CO2, Humidity and Temperature were added, plus the value of water PH

--- HTTP.cpp
+++ HTTP.cpp
@@ -1,7 +1,8 @@
 #include <HTTPClient.h>
 
 void HHTP_send_value(String serverName, String apiKeyValue, String location_id, String fans, String RGB, unsigned long pumpInterval, int tempThreshold, 
-                     , int humiThreshold, int CO2Threshold, float CO2Sensor, float HumiSensor, float TempSensor) { 
+                     , int humiThreshold, int CO2Threshold, float WaterPH, float CO2Internal, float HumiInternal, float TempInternal, float CO2External, 
+                     , float HumiExternal, float TempExternal) { 
   
   HTTPClient http;
   
@@ -13,12 +14,13 @@ void HHTP_send_value(String serverName, String apiKeyValue, String location_id, 
   
   // Prepare your HTTP POST request data
   String httpRequestData = "api_key=" + apiKeyValue + "&location_id=" + location_id + "&fans=" + fans + "&RGB=" + RGB + "&pump_interval=" + pumpInterval +
-                      + "&temp_threshold=" + tempThreshold + "&evap_threshold=" + humiThreshold + "&CO2_threshold=" + CO2Threshold + "&CO2sensor=" + CO2Sensor +
-                      + "&Humisensor=" + HumiSensor + "&Tempsensor=" + String(TempSensor) + "";
+                      + "&temp_threshold=" + tempThreshold + "&humi_threshold=" + humiThreshold + "&CO2_threshold=" + CO2Threshold + "&WaterPH=" + WaterPH +
+                      + "&CO2Internal=" + CO2Internal + "&HumiInternal=" + HumiInternal + "&TempInternal=" + String(TempInternal) + "&CO2External=" + CO2External + 
+                      + "&HumiExternal=" + HumiExternal + "&TempExternal=" + String(TempExternal) + "";
   
   // You can comment the httpRequestData variable above
   // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)    
-  //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=CO2&module=Modulo_1&shelving=Shelve_1&sector=Sector_1&value=1005.14";
+  // String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=CO2&module=Modulo_1&shelving=Shelve_1&sector=Sector_1&value=1005.14";
 
   Serial.print("httpRequestData: ");
   Serial.println(httpRequestData);
