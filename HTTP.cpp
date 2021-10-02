e0660cc0b231d2fc1bde91185d97058ca98a53ec
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-02 13:13:21 +0200

    Update HTTP.cpp

--- HTTP.cpp
+++ HTTP.cpp
@@ -1,6 +1,6 @@
 #include <HTTPClient.h>
 
-void HHTP_send_value(String serverName, String apiKeyValue, String location_id, String fans, String RGB, unsigned long pumpInterval, int tempThreshold, 
+void HHTP_send_value(String serverName, String apiKeyValue, String location_id, int fans, String RGB, unsigned long pumpInterval, int tempThreshold, 
                      , int humiThreshold, int CO2Threshold, float WaterPH, float CO2Internal, float HumiInternal, float TempInternal, float CO2External, 
                      , float HumiExternal, float TempExternal) { 
   
