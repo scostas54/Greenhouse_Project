098c0f724848f3312942051fd1c0b66f78be0129
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-02 10:57:03 +0200

    External Values + PH added
    The values for external CO2, Humidity and Temperature were added, plus the value of water PH

--- HTTP.h
+++ HTTP.h
@@ -1,2 +1,3 @@
 void HHTP_send_value(String serverName, String apiKeyValue, String location_id, String fans, String RGB, unsigned long pumpInterval, int tempThreshold, 
-                     , int humiThreshold, int CO2Threshold, float CO2Sensor, float HumiSensor, float TempSensor);
+                     , int humiThreshold, int CO2Threshold, float WaterPH, float CO2Internal, float HumiInternal, float TempInternal, float CO2External, 
+                     , float HumiExternal, float TempExternal);
