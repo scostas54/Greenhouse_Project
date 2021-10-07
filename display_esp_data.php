de4d8cdfb499ff67baeabf1a0530dc22a9efe33a
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-07 21:43:41 +0200

    Table Variables Updated

--- display_esp_data.php
+++ display_esp_data.php
@@ -15,31 +15,42 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, sensor, module, shelve, sector, value, reading_time FROM CO2 ORDER BY id DESC";
+$sql = "SELECT id, location_id, fans, RGB, pump_interval, temp_threshold, humidity_threshold, CO2_threshold, temp_internal, humidity_internal,
+        , CO2_internal, reading_time FROM CO2 ORDER BY id DESC";
 
 echo '<head>
-        <title><CO2></title>
+        <title><Measurements></title>
     </head>';
 
 echo '<table cellspacing="5" cellpadding="5">
       <tr> 
         <td>ID</td> 
-        <td>Sensor</td> 
-        <td>Module</td> 
-        <td>Shelve</td> 
-        <td>Sector</td>
-        <td>Value</td> 
+        <td>Location ID</td> 
+        <td>Fans</td> 
+        <td>RGB</td> 
+        <td>Pump Interval(ms)</td>
+        <td>Temp Threshold</td>
+        <td>Humidity Threshold</td>
+        <td>CO2 Threshold</td>
+        <td>Temp Internal</td>
+        <td>Humidity Internal</td>
+        <td>CO2 Internal</td>
         <td>Timestamp</td> 
       </tr>';
  
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
         $row_id = $row["id"];
-        $row_sensor = $row["sensor"];
-        $row_module = $row["module"];
-        $row_shelve = $row["shelve"];
-        $row_sector = $row["sector"]; 
-        $row_value = $row["value"]; 
+        $row_location_id = $row["location_id"];
+        $row_fans = $row["fans"];
+        $row_RGB = $row["RGB"];
+        $row_pump_interval = $row["pump_interval"]; 
+        $row_temp_threshold = $row["temp_threshold"];
+        $row_humidity_threshold = $row["humidity_threshold"];
+        $row_CO2_threshold = $row["CO2_threshold"];
+        $row_temp_internal = $row["temp_internal"];
+        $row_humidity_internal = $row["humidity_internal"];
+        $row_CO2_internal = $row["CO2_internal"];
         $row_reading_time = $row["reading_time"];
         // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
@@ -49,11 +60,16 @@ if ($result = $conn->query($sql)) {
       
         echo '<tr> 
                 <td>' . $row_id . '</td> 
-                <td>' . $row_sensor . '</td> 
-                <td>' . $row_module . '</td> 
-                <td>' . $row_shelve . '</td> 
-                <td>' . $row_sector . '</td>
-                <td>' . $row_value . '</td> 
+                <td>' . $row_location_id . '</td> 
+                <td>' . $row_fans . '</td> 
+                <td>' . $row_RGB . '</td> 
+                <td>' . $row_pump_interval . '</td>
+                <td>' . $row_temp_threshold . '</td>
+                <td>' . $row_humidity_threshold . '</td>
+                <td>' . $row_CO2_threshold . '</td>
+                <td>' . $row_temp_internal . '</td>
+                <td>' . $row_humidity_internal . '</td>
+                <td>' . $row_CO2_internal . '</td>                
                 <td>' . $row_reading_time . '</td> 
               </tr>';
     }
