a5814d7fcd5fe75dbaef69b897402095f3bea02b
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-10 15:31:25 +0200

    Working version

--- servidor.h
+++ servidor.h
@@ -3,16 +3,16 @@
 #include <WebServer.h>
 #include <analogWrite.h>
 
+void ghSetLED_Day();
+void ghSetLED_Dark();
+void ghSetLED_Last(int R, int G, int B);
+void HandleClient();
 void InitServer();
 void ConnectWiFi_STA();
 void ConnectWiFi_AP(bool useStaticIP);
 void WifiCheckConnection();
 void handleRoot();
 void handleNotFound();
-void HandleCliente();
-void ghSetLED_Day();
-void ghSetLED_Dark();
-void ghSetLED_Last(int R, int G, int B);
 int R_value();
 int G_value();
 int B_value();
