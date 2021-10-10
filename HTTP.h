434a1332faeb865440ece477460dec599549f07c
SCA <70905913+scostas54@users.noreply.github.com>
2021-10-10 15:24:59 +0200

    Working version

--- HTTP.h
+++ HTTP.h
@@ -1,3 +1,2 @@
 void HHTP_send_value(String serverName, String apiKeyValue, String location_id, int fans, String RGB, unsigned long pumpInterval, int tempThreshold, 
-                     , int humiThreshold, int CO2Threshold, float WaterPH, float CO2Internal, float HumiInternal, float TempInternal, float CO2External, 
-                     , float HumiExternal, float TempExternal);
+                     int humiThreshold, int CO2Threshold, float TempInternal, float HumiInternal, int CO2Internal);
